05f49753597ad7584055ab4f0c82ec0756eda002
Hani <haniyeh.jalayeri@gmail.com>
2025-08-25 22:59:58 -0400

    Add project files.

---
+++ Lab5bS2541/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Lab5bS2541
+{
+    partial class FormDoctorAndCompanions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDoctorAndCompanions));
+            this.groupBoxDoctor = new System.Windows.Forms.GroupBox();
+            this.picDoctor = new System.Windows.Forms.PictureBox();
+            this.txtFirstEpisode = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtSeries = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.txtPlayedBy = new System.Windows.Forms.TextBox();
+            this.lblFirstEpisode = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblSeries = new System.Windows.Forms.Label();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.lblPlayedBy = new System.Windows.Forms.Label();
+            this.lblDoctor = new System.Windows.Forms.Label();
+            this.cmbDoctor = new System.Windows.Forms.ComboBox();
+            this.groupBoxCompanions = new System.Windows.Forms.GroupBox();
+            this.lstCompanions = new System.Windows.Forms.ListBox();
+            this.picTardis = new System.Windows.Forms.PictureBox();
+            this.groupBoxDoctor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picDoctor)).BeginInit();
+            this.groupBoxCompanions.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picTardis)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxDoctor
+            // 
+            this.groupBoxDoctor.Controls.Add(this.picDoctor);
+            this.groupBoxDoctor.Controls.Add(this.txtFirstEpisode);
+            this.groupBoxDoctor.Controls.Add(this.txtAge);
+            this.groupBoxDoctor.Controls.Add(this.txtSeries);
+            this.groupBoxDoctor.Controls.Add(this.txtYear);
+            this.groupBoxDoctor.Controls.Add(this.txtPlayedBy);
+            this.groupBoxDoctor.Controls.Add(this.lblFirstEpisode);
+            this.groupBoxDoctor.Controls.Add(this.lblAge);
+            this.groupBoxDoctor.Controls.Add(this.lblSeries);
+            this.groupBoxDoctor.Controls.Add(this.lblYear);
+            this.groupBoxDoctor.Controls.Add(this.lblPlayedBy);
+            this.groupBoxDoctor.Controls.Add(this.lblDoctor);
+            this.groupBoxDoctor.Controls.Add(this.cmbDoctor);
+            this.groupBoxDoctor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxDoctor.Location = new System.Drawing.Point(36, 59);
+            this.groupBoxDoctor.Name = "groupBoxDoctor";
+            this.groupBoxDoctor.Size = new System.Drawing.Size(549, 732);
+            this.groupBoxDoctor.TabIndex = 0;
+            this.groupBoxDoctor.TabStop = false;
+            this.groupBoxDoctor.Text = "Doctor";
+            // 
+            // picDoctor
+            // 
+            this.picDoctor.Location = new System.Drawing.Point(287, 211);
+            this.picDoctor.Name = "picDoctor";
+            this.picDoctor.Size = new System.Drawing.Size(231, 291);
+            this.picDoctor.TabIndex = 12;
+            this.picDoctor.TabStop = false;
+            // 
+            // txtFirstEpisode
+            // 
+            this.txtFirstEpisode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFirstEpisode.Location = new System.Drawing.Point(22, 608);
+            this.txtFirstEpisode.Name = "txtFirstEpisode";
+            this.txtFirstEpisode.Size = new System.Drawing.Size(474, 40);
+            this.txtFirstEpisode.TabIndex = 11;
+            // 
+            // txtAge
+            // 
+            this.txtAge.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAge.Location = new System.Drawing.Point(185, 380);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(81, 40);
+            this.txtAge.TabIndex = 10;
+            // 
+            // txtSeries
+            // 
+            this.txtSeries.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSeries.Location = new System.Drawing.Point(185, 292);
+            this.txtSeries.Name = "txtSeries";
+            this.txtSeries.Size = new System.Drawing.Size(81, 40);
+            this.txtSeries.TabIndex = 9;
+            // 
+            // txtYear
+            // 
+            this.txtYear.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtYear.Location = new System.Drawing.Point(185, 211);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(81, 40);
+            this.txtYear.TabIndex = 8;
+            // 
+            // txtPlayedBy
+            // 
+            this.txtPlayedBy.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPlayedBy.Location = new System.Drawing.Point(173, 133);
+            this.txtPlayedBy.Name = "txtPlayedBy";
+            this.txtPlayedBy.Size = new System.Drawing.Size(345, 40);
+            this.txtPlayedBy.TabIndex = 7;
+            // 
+            // lblFirstEpisode
+            // 
+            this.lblFirstEpisode.AutoSize = true;
+            this.lblFirstEpisode.Location = new System.Drawing.Point(16, 546);
+            this.lblFirstEpisode.Name = "lblFirstEpisode";
+            this.lblFirstEpisode.Size = new System.Drawing.Size(250, 33);
+            this.lblFirstEpisode.TabIndex = 6;
+            this.lblFirstEpisode.Text = "First Full Episode:";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(16, 387);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(171, 33);
+            this.lblAge.TabIndex = 5;
+            this.lblAge.Text = "Age at start:";
+            // 
+            // lblSeries
+            // 
+            this.lblSeries.AutoSize = true;
+            this.lblSeries.Location = new System.Drawing.Point(16, 299);
+            this.lblSeries.Name = "lblSeries";
+            this.lblSeries.Size = new System.Drawing.Size(106, 33);
+            this.lblSeries.TabIndex = 4;
+            this.lblSeries.Text = "Series:";
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(16, 218);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(85, 33);
+            this.lblYear.TabIndex = 3;
+            this.lblYear.Text = "Year:";
+            // 
+            // lblPlayedBy
+            // 
+            this.lblPlayedBy.AutoSize = true;
+            this.lblPlayedBy.Location = new System.Drawing.Point(16, 140);
+            this.lblPlayedBy.Name = "lblPlayedBy";
+            this.lblPlayedBy.Size = new System.Drawing.Size(151, 33);
+            this.lblPlayedBy.TabIndex = 2;
+            this.lblPlayedBy.Text = "Played by:";
+            // 
+            // lblDoctor
+            // 
+            this.lblDoctor.AutoSize = true;
+            this.lblDoctor.Location = new System.Drawing.Point(16, 61);
+            this.lblDoctor.Name = "lblDoctor";
+            this.lblDoctor.Size = new System.Drawing.Size(109, 33);
+            this.lblDoctor.TabIndex = 1;
+            this.lblDoctor.Text = "Doctor:";
+            // 
+            // cmbDoctor
+            // 
+            this.cmbDoctor.DisplayMember = "ACTOR";
+            this.cmbDoctor.FormattingEnabled = true;
+            this.cmbDoctor.Location = new System.Drawing.Point(173, 53);
+            this.cmbDoctor.Name = "cmbDoctor";
+            this.cmbDoctor.Size = new System.Drawing.Size(345, 41);
+            this.cmbDoctor.TabIndex = 0;
+            this.cmbDoctor.TabStop = false;
+            this.cmbDoctor.ValueMember = "ACTOR";
+            // 
+            // groupBoxCompanions
+            // 
+            this.groupBoxCompanions.Controls.Add(this.lstCompanions);
+            this.groupBoxCompanions.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxCompanions.Location = new System.Drawing.Point(608, 59);
+            this.groupBoxCompanions.Name = "groupBoxCompanions";
+            this.groupBoxCompanions.Size = new System.Drawing.Size(723, 732);
+            this.groupBoxCompanions.TabIndex = 1;
+            this.groupBoxCompanions.TabStop = false;
+            this.groupBoxCompanions.Text = "Companions and their first episode with this Doctor";
+            // 
+            // lstCompanions
+            // 
+            this.lstCompanions.FormattingEnabled = true;
+            this.lstCompanions.ItemHeight = 33;
+            this.lstCompanions.Location = new System.Drawing.Point(26, 53);
+            this.lstCompanions.Name = "lstCompanions";
+            this.lstCompanions.Size = new System.Drawing.Size(674, 664);
+            this.lstCompanions.TabIndex = 0;
+            // 
+            // picTardis
+            // 
+            this.picTardis.Image = ((System.Drawing.Image)(resources.GetObject("picTardis.Image")));
+            this.picTardis.Location = new System.Drawing.Point(1337, 120);
+            this.picTardis.Name = "picTardis";
+            this.picTardis.Size = new System.Drawing.Size(195, 359);
+            this.picTardis.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picTardis.TabIndex = 2;
+            this.picTardis.TabStop = false;
+            // 
+            // FormDoctorAndCompanions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1955, 808);
+            this.Controls.Add(this.picTardis);
+            this.Controls.Add(this.groupBoxCompanions);
+            this.Controls.Add(this.groupBoxDoctor);
+            this.Name = "FormDoctorAndCompanions";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Doctor and Companions";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.FormDoctorAndCompanions_Load);
+            this.groupBoxDoctor.ResumeLayout(false);
+            this.groupBoxDoctor.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picDoctor)).EndInit();
+            this.groupBoxCompanions.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picTardis)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxDoctor;
+        private System.Windows.Forms.GroupBox groupBoxCompanions;
+        private System.Windows.Forms.ComboBox cmbDoctor;
+        private System.Windows.Forms.Label lblDoctor;
+        private System.Windows.Forms.Label lblPlayedBy;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblSeries;
+        private System.Windows.Forms.Label lblFirstEpisode;
+        private System.Windows.Forms.TextBox txtSeries;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.TextBox txtPlayedBy;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtFirstEpisode;
+        private System.Windows.Forms.PictureBox picDoctor;
+        private System.Windows.Forms.ListBox lstCompanions;
+        private System.Windows.Forms.PictureBox picTardis;
+    }
+}
+
---
+++ Lab5bS2541/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Data.Entity;   
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lab5bS2541
+{
+    /// <summary>
+    /// Windows Forms that displays Doctor Who data using Entity Framework and LINQ.
+    /// Left side: doctor selector + details & photo.
+    /// Right sid: companions list 
+    /// </summary>
+    public partial class FormDoctorAndCompanions : Form
+    {
+        private COMP10204W25_LAB5Entities dbContext;
+        /// <summary>
+        /// Initializes the Doctor/Companion form
+        /// </summary>
+        public FormDoctorAndCompanions()
+        {
+            InitializeComponent();
+            CreateMenu();
+            //readonly field in the form
+            txtPlayedBy.ReadOnly = txtYear.ReadOnly = txtSeries.ReadOnly = txtAge.ReadOnly = txtFirstEpisode.ReadOnly = true;
+            txtPlayedBy.TabStop = txtYear.TabStop = txtSeries.TabStop = txtAge.TabStop = txtFirstEpisode.TabStop = false;
+            // Configure ListBox line in a way to have sapce
+            lstCompanions.DrawMode = DrawMode.OwnerDrawFixed;
+            // Fixed height:2 text lines + extra gap 
+            lstCompanions.ItemHeight = (int)(lstCompanions.Font.GetHeight() * 2) + 14;
+            // Draw each item in lsitbox
+            lstCompanions.DrawItem += (s, e) =>
+            {
+                if (e.Index < 0) return;
+                using (var b = new SolidBrush(SystemColors.Window))
+                    e.Graphics.FillRectangle(b, e.Bounds);
+                string text = (string)lstCompanions.Items[e.Index];
+                var rect = new Rectangle(e.Bounds.X + 4, e.Bounds.Y + 3,
+                                         e.Bounds.Width - 8, e.Bounds.Height - 6);
+                TextRenderer.DrawText(e.Graphics, text, lstCompanions.Font, rect,
+                                      SystemColors.WindowText, TextFormatFlags.WordBreak);
+            };
+            picDoctor.SizeMode = PictureBoxSizeMode.Zoom;
+            this.Load += FormDoctorAndCompanions_Load;
+            this.cmbDoctor.SelectedIndexChanged += cmbDoctor_SelectedIndexChanged;
+
+            cmbDoctor.DropDownStyle = ComboBoxStyle.DropDownList;
+            lstCompanions.HorizontalScrollbar = true;
+        }
+        /// <summary>
+        /// Creates the form's main menu 
+        /// </summary>
+        private void CreateMenu()
+        {
+            //create menustrip
+            var menu = new MenuStrip();
+            var file = new ToolStripMenuItem("&File");
+
+            // Exit with Ctrl+Q
+            var exitItem = new ToolStripMenuItem("E&xit", image: null, onClick: (s, e) => this.Close())
+            {
+                ShortcutKeys = Keys.Control | Keys.Q,
+                ShowShortcutKeys = true
+            };
+
+            file.DropDownItems.Add(exitItem);
+            menu.Items.Add(file);
+
+            this.MainMenuStrip = menu;   // mark as the primary menu for the form
+            this.Controls.Add(menu);     // display it
+        }
+        /// <summary>
+        /// Form load handler.
+        /// Creates the DbContext, fetches doctors, and binds the ComboBox to show numbers 1 to 13.
+        /// </summary>
+        /// <param name="sender">The control that raised the event</param>
+        /// <param name="e">An event arguments for the Click event</param>
+        private void FormDoctorAndCompanions_Load(object sender, EventArgs e)
+        {
+            dbContext = new COMP10204W25_LAB5Entities();
+
+            // get ids 
+            var doctorRows = dbContext.DOCTORs
+                                      .OrderBy(d => d.DOCTORID)
+                                      .Select(d => new { d.DOCTORID, d.ACTOR })
+                                      .ToList(); 
+
+            // display just the number 1to13
+            cmbDoctor.DisplayMember = "DOCTORID";
+            cmbDoctor.ValueMember = "DOCTORID";
+            cmbDoctor.DataSource = doctorRows;
+            if (cmbDoctor.Items.Count > 0)
+            {
+                cmbDoctor.SelectedIndex = 0;
+                // ensure details populate on first load
+                cmbDoctor_SelectedIndexChanged(cmbDoctor, EventArgs.Empty);
+            }
+        }
+        /// <summary>
+        /// execute the selected doctor changes
+        /// </summary>
+        /// <param name="sender">The control that raised the event</param>
+        /// <param name="e">An event arguments for the Click event</param>
+        private void cmbDoctor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!(cmbDoctor.SelectedValue is int doctorId)) return;
+
+            // Load the doctor and its episode 
+            var doc = dbContext.DOCTORs
+                .Include(d => d.EPISODE)
+                .FirstOrDefault(d => d.DOCTORID == doctorId);
+            if (doc == null) return; 
+            // Left-side fields
+            txtPlayedBy.Text = doc.ACTOR;
+            txtSeries.Text = doc.SERIES.ToString();
+            txtAge.Text = doc.AGE.ToString();
+            txtYear.Text = doc.EPISODE.SEASONYEAR.ToString();
+            txtFirstEpisode.Text = doc.EPISODE.TITLE;
+            // Photo 
+            picDoctor.Image = null;
+            try
+            {
+                using (var ms = new MemoryStream(doc.PICTURE))
+                    picDoctor.Image = Image.FromStream(ms);
+            }
+            catch (Exception ex){ 
+                Console.WriteLine(ex.Message); 
+            }
+            // Companions 
+            var companions = dbContext.COMPANIONs
+                .Where(c => c.DOCTORID == doctorId)
+                .Include(c => c.EPISODE)
+                .OrderBy(c => c.EPISODE.SEASONYEAR) //orderby year
+                .Select(c => new
+                {
+                    c.NAME,
+                    c.ACTOR,
+                    Title = c.EPISODE.TITLE,
+                    Year = c.EPISODE.SEASONYEAR
+                })
+                .ToList();
+            var lines = companions
+                .Select(x => $"{x.NAME} ({x.ACTOR})\r\n\"{x.Title}\" ({x.Year})")
+                .ToList();
+
+            lstCompanions.DataSource = lines;
+            lstCompanions.Focus();
+
+        }
+        /// <summary>
+        /// Ensures the DbContext is disposed when the form closes.
+        /// </summary>
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            dbContext?.Dispose();
+            base.OnFormClosed(e);
+        }
+    }
+}
